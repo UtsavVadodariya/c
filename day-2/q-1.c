e57481e641c9a0fe56203f57e4e09f03f4ebf8ec
UtsavVadodariya <utsavvadodariya2008@gmail.com>
2024-03-06 16:08:57 +0530

    oo

---
+++ day-2/q-1.c
@@ -0,0 +1,9 @@
+//Write a Program to print your name, age, and school
+
+#include <stdio.h>
+main()
+{
+    printf("Name :- Vadodariya Utsav");
+    printf("\nAge :- 17");
+    printf("\nSchool :- Dev International School");
+}
