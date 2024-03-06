c9dc3d818d9f620baa39e23606d0276266f14207
UtsavVadodariya <utsavvadodariya2008@gmail.com>
2024-03-06 16:22:09 +0530

    ll

---
+++ day-8.c
@@ -0,0 +1,18 @@
+// Write a program that takes two integers as input and calculates their sum.
+
+#include<stdio.h>
+
+main(){
+
+    int a,b,c;
+
+    printf("enter the first value :");
+    scanf("%d",&a);
+
+    printf("enter the second value :");
+    scanf("%d",&b);
+
+    c=a+b;
+    printf("total of your numbers :%d",c);
+    
+}
