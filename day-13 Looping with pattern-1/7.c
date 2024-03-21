01759018f3c4377cc9b052f56fe7a82499316694
UtsavVadodariya <162439323+UtsavVadodariya@users.noreply.github.com>
2024-03-21 15:38:11 +0530

    Looping with pattern-1

---
+++ day-13 Looping with pattern-1/7.c
@@ -0,0 +1,24 @@
+// Write a Program to print the below pattern using nested for loop.
+// A
+// B A
+// C B A
+// D C B A
+// E D C B A
+
+
+#include<stdio.h>
+
+main()
+{
+     for (char i = 'A'; i <='E'; i++)
+    {
+        for (int j = i; j >='A'; j--)
+        {
+            printf("%c ",j);
+        }
+        printf("\n");
+        
+    }
+    
+    
+}
