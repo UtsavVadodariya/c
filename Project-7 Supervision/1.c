4a30c4035e215762fc1718fd4b5b1f50694a34f6
UtsavVadodariya <162439323+UtsavVadodariya@users.noreply.github.com>
2024-04-18 16:04:49 +0530

    Supervision

--- Project-7 Supervision/1.c
+++ Project-7 Supervision/1.c
@@ -1,15 +1,15 @@
-// Q.1 Write a Program to create a car Record System with an Array of objects for a given N number of cars using structure. Consider the following attributes in the car structure:
-// - car_id
-// - car_title
-// - car_author
-// - car_genre
-// - car_publisher
-// - car_publication_year
-// - car_price
+// Q.1 Write a Program to create a book Record System with an Array of objects for a given N number of books using structure. Consider the following attributes in the book structure:
+// - book_id
+// - book_title
+// - book_author
+// - book_genre
+// - book_publisher
+// - book_publication_year
+// - book_price
 
 #include <stdio.h>
 
-struct car
+struct book
 {
     int id;
     char title[100];
@@ -18,42 +18,42 @@ struct car
     char publisher[100];
     int publication_year;
     int price;
-} car[];
+} book[];
 
 int main()
 {
     int n;
 
-    printf("Enter The SIze Of cars :");
+    printf("Enter The SIze Of books :");
     scanf("%d", &n);
     printf("\n");
 
     for (int i = 0; i < n; i++)
     {
-        printf("Enter Id Of car :");
-        scanf("%d", &car[i].id);
+        printf("Enter Id Of book :");
+        scanf("%d", &book[i].id);
 
-        printf("Enter Title Of car :");
-        scanf("%s", &car[i].title);
+        printf("Enter Title Of book :");
+        scanf("%s", &book[i].title);
 
-        printf("Enter Author Of car :");
-        scanf("%s", &car[i].author);
+        printf("Enter Author Of book :");
+        scanf("%s", &book[i].author);
 
-        printf("Enter Genre Of car :");
-        scanf("%s", &car[i].genre);
+        printf("Enter Genre Of book :");
+        scanf("%s", &book[i].genre);
 
-        printf("Enter Publisher Of car :");
-        scanf("%s", &car[i].publisher);
+        printf("Enter Publisher Of book :");
+        scanf("%s", &book[i].publisher);
 
-        printf("Enter Publication_year Of car :");
-        scanf("%d", &car[i].publication_year);
+        printf("Enter Publication_year Of book :");
+        scanf("%d", &book[i].publication_year);
     }
 
     printf("\n");
 
     for (int i = 0; i < n; i++)
     {
-        printf("Id : %d\n Title : %s\n Author : %s\n Genre : %s\n Publisher : %s\n Publication_year : %d\n", car[i].id, car[i].title, car[i].author, car[i].genre, car[i].publisher, car[i].publication_year);
+        printf("Id : %d\n Title : %s\n Author : %s\n Genre : %s\n Publisher : %s\n Publication_year : %d\n", book[i].id, book[i].title, book[i].author, book[i].genre, book[i].publisher, book[i].publication_year);
         printf("\n");
     }
     
