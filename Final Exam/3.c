403fc4e27431a3510a1269069bcb5180cd04bb23
UtsavVadodariya <162439323+UtsavVadodariya@users.noreply.github.com>
2024-04-24 15:43:30 +0530

    Final Exam

---
+++ Final Exam/3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main() {
+    int arr[100];
+    int n, i;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("All odd numbers in the array : ");
+    for (i = 0; i < n; i++) {
+        if (arr[i] % 2 != 0) {
+            printf("%d ", arr[i]);
+        }
+    }
+
+}
